dd6dae88ea5775aba315396c19760d03419a09b0
nk12U <nxsk12@gmail.com>
2025-09-24 22:55:55 +0900

    Initial commit

--- tb_tsmccim.sv
+++ tb_tsmccim.sv
@@ -1,169 +1,180 @@
-`timescale 1ns/1ps
+`timescale 1ps/1ps
 
 module tb_tsmccim;
+    // set DUT parameters as local parameter
+    localparam CORE_NUM = 16;
+    localparam XIN_BIT_WIDTH = 11;
+    localparam MEM_BIT_WIDTH = 8;
+    localparam CORE_DOUT_BIT_WIDTH = XIN_BIT_WIDTH + MEM_BIT_WIDTH - 1; // 18
+    localparam MEM_ADR_WIDTH = 2;
+    localparam OUTPUT_BIT_WIDTH = CORE_DOUT_BIT_WIDTH + $clog2(CORE_NUM); // 22
 
-    reg CLK, NRST;
-    reg [3:0] BANKA, BANKB;
-    reg [1:0] ADRA, ADRB;
-    reg [7:0] D;
-    reg WEB, REB, ENCB;
-    reg [175:0] XIN;
-    wire [21:0] Q;
+    parameter clk_peri = 10_000; // 100MHz, clock period
+    
+    // expected value
+    parameter [OUTPUT_BIT_WIDTH-1:0] EXPECTED_Q_VALUE = 22'h009eb0;
+    reg clk;
+    reg nrst;
+    reg encb;
+    reg web;
+    reg [$clog2(CORE_NUM)-1:0] banka;
+    reg [MEM_ADR_WIDTH-1:0] adra;
+    reg [MEM_BIT_WIDTH-1:0] d;
+    reg reb;
+    reg [$clog2(CORE_NUM)-1:0] bankb;
+    reg [MEM_ADR_WIDTH-1:0] adrb;
+    reg [(CORE_NUM*XIN_BIT_WIDTH)-1:0] xin;
+    wire [OUTPUT_BIT_WIDTH-1:0] q;
 
-    tsmccim16x8x11m1 dut (
-        .CLK(CLK), .NRST(NRST),
-        .BANKA(BANKA), .ADRA(ADRA), .D(D), .WEB(WEB),
-        .BANKB(BANKB), .ADRB(ADRB), .REB(REB),
-        .ENCB(ENCB), .XIN(XIN),
-        .Q(Q)
+    // Generate Clock
+    initial begin
+        #0 clk = 1'b0;
+        forever begin
+            #(clk_peri/2) clk = ~clk;
+        end
+    end
+
+    // DUT
+    tsmccim16x8x11m1 #(
+        .CORE_NUM(CORE_NUM),
+        .XIN_BIT_WIDTH(XIN_BIT_WIDTH),
+        .MEM_BIT_WIDTH(MEM_BIT_WIDTH),
+        .MEM_ADR_WIDTH(MEM_ADR_WIDTH),
+    ) DUT (
+        .CLK(clk),
+        .NRST(nrst),
+        .ENCB(encb),
+        .WEB(web),
+        .BANKA(banka),
+        .ADRA(adra),
+        .D(d),
+        .REB(reb),
+        .BANKB(bankb),
+        .ADRB(adrb),
+        .XIN(xin),
+        .Q(q)
     );
-
-    initial CLK = 0;
-    always #5 CLK = ~CLK;
 
     initial begin
         $display("--- Simulation Start ---");
-        {BANKA, BANKB, ADRA, ADRB, D} = 0;
-        {WEB, REB, ENCB} = {1'b1, 1'b1, 1'b1};
-        NRST = 0;
+        {banka, bankb, adra, adrb, d} = 0;
+        {web, reb, encb} = {1'b1, 1'b1, 1'b1};
+        nrst = 0;
         #20;
-        NRST = 1;
+        nrst = 1;
         #10;
 
         $display("\n--- 1. Write Sequence ---");
-        WEB <= 0;
-        @(negedge CLK);
+        web = 0;
+        @(negedge clk);
 
         $display("Writing 8'hAA to BANK 0, ADDR 0");
-        BANKA <= 4'd0; ADRA <= 2'd0; D <= 8'hAA;
-        @(negedge CLK);
+        banka = 4'd0; adra = 2'd0; d = 8'hAA;
+        @(negedge clk);
 
         $display("Writing 8'hB1 to BANK 0, ADDR 1");
-        BANKA <= 4'd0; ADRA <= 2'd1; D <= 8'hB1;
-        @(negedge CLK);
+        banka = 4'd0; adra = 2'd1; d = 8'hB1;
+        @(negedge clk);
 
         $display("Writing 8'hC2 to BANK 0, ADDR 2");
-        BANKA <= 4'd0; ADRA <= 2'd2; D <= 8'hC2;
-        @(negedge CLK);
-        
-        WEB <= 1;
-        {BANKA, ADRA, D} = 0;
-        repeat (4)@(negedge CLK);
+        banka = 4'd0; adra = 2'd2; d = 8'hC2;
+        @(negedge clk);
+
+        $display("Writing 8'hD3 to BANK 0, ADDR 3");
+        banka = 4'd0; adra = 2'd3; d = 8'hD3;
+        @(negedge clk);
+
+        web = 1;
+        {banka, adra, d} = 0;
+        repeat (4) @(negedge clk);
 
 
         $display("\n--- 2. Read Sequence ---");
-        REB <= 0;
+        reb = 0;
 
         $display("Reading from BANK 0, ADDR 0...");
-        BANKB <= 4'd0; ADRB <= 2'd0;
+        
+        bankb = 4'd0; 
+        adrb = 2'd0;
+        @(negedge clk);
 
-        @(negedge CLK);
+        adrb = 2'd1;
+        @(negedge clk);
 
-        ADRB <= 2'd1;
-        @(negedge CLK);
+        adrb = 2'd2;
+        @(negedge clk);
 
-        ADRB <= 2'd2;
-        @(negedge CLK);
+        adrb = 2'd3;
+        @(negedge clk);
 
-        REB <= 1;
-        repeat (4) @(negedge CLK);
+        reb = 1;
+        repeat (4) @(negedge clk);
 
+        $display("\n--- 3. MAC Operation Sequence ---");
         // MAC Operation
-        WEB <= 1'b0;
-        ADRA <= 2'b0;
+        web = 1'b0;
+        adra = 2'b0;
 
-        D = 8'd34;
-        BANKA = 4'd0;
+        for (int i = 0; i < CORE_NUM; i++) begin
+            @(negedge clk);
+            case (i)
+                0:  d = 8'd34;
+                1:  d = 8'd64;
+                2:  d = 8'd240;
+                3:  d = 8'd242;
+                4:  d = 8'd143;
+                5:  d = 8'd109;
+                6:  d = 8'd166;
+                7:  d = 8'd233;
+                8:  d = 8'd105;
+                9:  d = 8'd148;
+                10: d = 8'd74;
+                11: d = 8'd182;
+                12: d = 8'd28;
+                13: d = 8'd100;
+                14: d = 8'd22;
+                15: d = 8'd235;
+                default: d = 8'd0;
+            endcase
+            banka = i;
+        end
 
-        @(negedge CLK);
-        D = 8'd64;
-        BANKA = 4'd1;
+        @(negedge clk);
+        web = 1'b1;
 
-        @(negedge CLK);
-        D = 8'd240;
-        BANKA = 4'd2;
+        repeat (4) @(negedge clk);
 
-        @(negedge CLK);
-        D = 8'd242;
-        BANKA = 4'd3;
+        adrb = 2'b0;
+        encb = 1'b0;
 
-        @(negedge CLK);
-        D = 8'd143;
-        BANKA = 4'd4;
-
-        @(negedge CLK);
-        D = 8'd109;
-        BANKA = 4'd5;
-
-        @(negedge CLK);
-        D = 8'd166;
-        BANKA = 4'd6;
-
-        @(negedge CLK);
-        D = 8'd233;
-        BANKA = 4'd7;
-
-        @(negedge CLK);
-        D = 8'd105;
-        BANKA = 4'd8;
-
-        @(negedge CLK);
-        D = 8'd148;
-        BANKA = 4'd9;
-
-        @(negedge CLK);
-        D = 8'd74;
-        BANKA = 4'd10;
-
-        @(negedge CLK);
-        D = 8'd182;
-        BANKA = 4'd11;
-
-        @(negedge CLK);
-        D = 8'd28;
-        BANKA = 4'd12;
-
-        @(negedge CLK);
-        D = 8'd100;
-        BANKA = 4'd13;
-
-        @(negedge CLK);
-        D = 8'd22;
-        BANKA = 4'd14;
-
-        @(negedge CLK);
-        D = 8'd235;
-        BANKA = 4'd15;
-
-        @(negedge CLK);
-        WEB <= 1'b1;
-
-        repeat (4) @(negedge CLK);
-
-        ADRB <= 2'b0;
-        ENCB <= 1'b0;
-
-        XIN[10:0]  = 11'd467;
-        XIN[21:11] = 11'd451;
-        XIN[32:22] = 11'd1645;
-        XIN[43:33] = 11'd818;
-        XIN[54:44] = 11'd1262;
-        XIN[65:55] = 11'd1105;
-        XIN[76:66] = 11'd1832;
-        XIN[87:77] = 11'd1011;
-        XIN[98:88] = 11'd871;
-        XIN[109:99]= 11'd464;
-        XIN[120:110]=11'd1348;
-        XIN[131:121]=11'd1808;
-        XIN[142:132]=11'd1282;
-        XIN[153:143]=11'd987;
-        XIN[164:154]=11'd680;
-        XIN[175:165]=11'd997;
+        // xin 11bit data input
+        for (int i = 0; i < CORE_NUM; i++) begin
+            case(i)
+                0:  xin[(i+1)*XIN_BIT_WIDTH-1 -: XIN_BIT_WIDTH] = XIN_BIT_WIDTH'(467);
+                1:  xin[(i+1)*XIN_BIT_WIDTH-1 -: XIN_BIT_WIDTH] = XIN_BIT_WIDTH'(451);
+                2:  xin[(i+1)*XIN_BIT_WIDTH-1 -: XIN_BIT_WIDTH] = XIN_BIT_WIDTH'(1645);
+                3:  xin[(i+1)*XIN_BIT_WIDTH-1 -: XIN_BIT_WIDTH] = XIN_BIT_WIDTH'(818);
+                4:  xin[(i+1)*XIN_BIT_WIDTH-1 -: XIN_BIT_WIDTH] = XIN_BIT_WIDTH'(1262);
+                5:  xin[(i+1)*XIN_BIT_WIDTH-1 -: XIN_BIT_WIDTH] = XIN_BIT_WIDTH'(1105);
+                6:  xin[(i+1)*XIN_BIT_WIDTH-1 -: XIN_BIT_WIDTH] = XIN_BIT_WIDTH'(1832);
+                7:  xin[(i+1)*XIN_BIT_WIDTH-1 -: XIN_BIT_WIDTH] = XIN_BIT_WIDTH'(1011);
+                8:  xin[(i+1)*XIN_BIT_WIDTH-1 -: XIN_BIT_WIDTH] = XIN_BIT_WIDTH'(871);
+                9:  xin[(i+1)*XIN_BIT_WIDTH-1 -: XIN_BIT_WIDTH] = XIN_BIT_WIDTH'(464);
+                10: xin[(i+1)*XIN_BIT_WIDTH-1 -: XIN_BIT_WIDTH] = XIN_BIT_WIDTH'(1348);
+                11: xin[(i+1)*XIN_BIT_WIDTH-1 -: XIN_BIT_WIDTH] = XIN_BIT_WIDTH'(1808);
+                12: xin[(i+1)*XIN_BIT_WIDTH-1 -: XIN_BIT_WIDTH] = XIN_BIT_WIDTH'(1282);
+                13: xin[(i+1)*XIN_BIT_WIDTH-1 -: XIN_BIT_WIDTH] = XIN_BIT_WIDTH'(987);
+                14: xin[(i+1)*XIN_BIT_WIDTH-1 -: XIN_BIT_WIDTH] = XIN_BIT_WIDTH'(680);
+                15: xin[(i+1)*XIN_BIT_WIDTH-1 -: XIN_BIT_WIDTH] = XIN_BIT_WIDTH'(997);
+                default: xin[(i+1)*XIN_BIT_WIDTH-1 -: XIN_BIT_WIDTH] = XIN_BIT_WIDTH'(0);
+            endcase
+        end
 
         repeat (4) @(negedge CLK);
 
         $display("\n--- Simulation Finish ---");
-        $stop;
+        $finish;
+
     end
 
     // initial begin
